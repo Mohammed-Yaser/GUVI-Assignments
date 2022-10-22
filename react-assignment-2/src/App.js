3bd5ac5c36d5546256d2a74cdff75a2059e6543f
Mohammed-Yaser <78248958+Mohammed-Yaser@users.noreply.github.com>
2022-10-22 15:42:51 +0530

    Add files via upload

---
+++ react-assignment-2/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import React from "react";
+import Nav from "./Nav";
+import Content from "./Content";
+import Products from "./Products";
+import Footer from "./Footer";
+function App() {
+  return (
+    <div>
+      <Nav />
+      <Content />
+      <Products />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
